11108c2c0dd1709cdc3bc2f4a198cc34319ec9ad
Tarik-Alaiwi <54720296+Tarik-Alaiwi@users.noreply.github.com>
2023-10-21 21:32:45 +0200

    Delete 22 directory

--- 22/KontoBankowe.java
+++
@@ -1,9 +0,0 @@
-public class KontoBankowe {
-    public double saldo;
-    public void wplac(double kwota){
-        saldo += kwota;
-    }
-    public void wyplac(double kwota){
-        saldo -= kwota;
-    }
-}
--- 22/Pies.java
+++
@@ -1,9 +0,0 @@
-public class Pies {
-    public String nazwa;
-    public String rasa;
-    public int wiek;
-
-    public void dajGlos(){
-        System.out.println("Hau Hau");
-    }
-}
